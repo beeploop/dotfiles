254d38f5f2cd9ea12283b2bb2d991c246f7bbddc
John Loyd Mulit <jlmulit68@gmail.com>
2026-07-25 00:32:47 +0800

    chore: make nvim transparent bg

--- nvim/.config/nvim/lua/beeploop/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/beeploop/plugins/colorscheme.lua
@@ -8,4 +8,19 @@ do
 	require("tokyodark").setup()
 
 	vim.cmd.colorscheme("tokyodark")
+
+	local transparent = function()
+		local groups = {
+			"Normal",
+			"NormalFloat",
+			"SignColumn",
+			"EndOfBuffer",
+		}
+
+		for _, group in ipairs(groups) do
+			vim.api.nvim_set_hl(0, group, { bg = "none" })
+		end
+	end
+
+	transparent()
 end
